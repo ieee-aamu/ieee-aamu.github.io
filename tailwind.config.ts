e5e233b3c40d3fa27c0010049a155ab9c62fbdb3
Sudeep Joshi <joshisudeep456@gmail.com>
2025-03-16 09:39:14 -0500

    importing modules

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        maroon: {
+          50: "#fdf6f5",
+          100: "#f9e5e3",
+          200: "#eebdb8",
+          300: "#e2918a",
+          400: "#d55e55",
+          500: "#9e3b2d", 
+          600: "#872e23",
+          700: "#6f221b",
+          800: "#5a1914",
+          900: "#49130f",
+        },
+        white: "#ffffff",
+      },
+      fontFamily: {
+        sans: ["Verdana", "Geneva", "Tahoma", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
