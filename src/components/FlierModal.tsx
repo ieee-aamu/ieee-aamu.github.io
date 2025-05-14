5545828679dbcb7a40c2b4520032441d2c8ece07
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2025-05-13 23:19:21 -0500

    feat: made flier modal responsive

--- src/components/FlierModal.tsx
+++ src/components/FlierModal.tsx
@@ -1,28 +1,36 @@
 "use client";
 import React, { useState } from "react";
 import { X } from "lucide-react";
+import Image from "next/image";
 
 export default function FlierModal() {
-  // New state for flier modal
+  // state for flier modal
   const [showFlier, setShowFlier] = useState(true);
 
   if (!showFlier) return null;
-  // Flier Modal Component
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative max-w-4xl w-full max-h-[90vh] overflow-hidden">
-        <button
-          onClick={() => setShowFlier(false)}
-          className="absolute top-2 right-2 z-60 bg-white rounded-full p-2 hover:bg-gray-100 transition"
-        >
-          <X className="h-6 w-6 text-black" />
-        </button>
-        <img
-          src="images/flier.jpg"
-          alt="IEEE AAMU Flier"
-          className="max-w-full max-h-full object-contain"
-        />
+    <div
+      className="fixed inset-0 z-50 bg-black/50 w-full h-screen overflow-hidden"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="w-full h-full grid items-center overflow-y-auto p-5">
+        <div className="relative max-w-4xl mx-auto">
+          <button
+            onClick={() => setShowFlier(false)}
+            className="absolute top-2 right-2 z-60 bg-white rounded-full p-2 hover:bg-gray-100 transition"
+          >
+            <X className="h-6 w-6 text-black" />
+          </button>
+          <Image
+            src="/images/flier.jpg"
+            alt="IEEE AAMU Flier"
+            width={1000}
+            height={1000}
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );
